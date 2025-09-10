2fb9004d35e581100fb310323cf28645fcdce5e3
OSH <42954693+oshosh@users.noreply.github.com>
2025-09-10 16:36:06 +0900

    Create RemoteHeaderClient.tsx

---
+++ src/components/RemoteHeaderClient.tsx
@@ -0,0 +1,22 @@
+// src/components/RemoteHeaderClient.tsx
+"use client";
+
+import { useEffect, useState } from "react";
+
+type Props = { title?: string };
+
+export default function RemoteHeaderClient(props: Props) {
+  const [Comp, setComp] = useState<any>(null);
+
+  useEffect(() => {
+    let mounted = true;
+    (async () =>
+      const mod = await import("shop/Header");
+      if (mounted) setComp(() => mod.default);
+    })();
+    return () => { mounted = false; };
+  }, []);
+
+  if (!Comp) return <div>Remote Header 로딩중…</div>;
+  return <Comp {...props} />;
+}
