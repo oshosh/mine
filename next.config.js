b6ba5c0745804ee717847153cf721186676aa19e
OSH <42954693+oshosh@users.noreply.github.com>
2025-09-10 14:41:21 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-};
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "host",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: {
+          shop: "shop@https://remotea-z1jl.vercel.app/_next/static/chunks/remoteEntry.js"
+        },
+        exposes: {
+          "./HostWidget": "./components/HostWidget.tsx"
+        },
+        shared: {
+          react: { singleton: true, requiredVersion: false },
+          "react-dom": { singleton: true, requiredVersion: false }
+        }
+      })
+    );
+    return config;
+  }
+};
