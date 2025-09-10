2d4ab9074758a1d94cf7da1441513930a40618e8
OSH <42954693+oshosh@users.noreply.github.com>
2025-09-10 15:25:37 +0900

    Update legacy.tsx

--- src/pages/legacy.tsx
+++ src/pages/legacy.tsx
@@ -1,14 +1,14 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const RemoteHeader: any = dynamic(() => import('shop/Header'), {
+const RemoteHeader = dynamic(() => import("shop/Header"), {
   ssr: false,
   loading: () => <div>Remote Header 로딩중…</div>,
 });
 
-export default function TestPage() {
+export default function LegacyPage() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Host Pages Router (/test)</h1>
+      <h1>Host Pages Router (/legacy)</h1>
       <RemoteHeader title="호스트에서 불러옴" />
     </div>
   );
